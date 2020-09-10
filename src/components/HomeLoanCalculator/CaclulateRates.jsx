fd495edd18ce4292c088bcb2cd0113ab7d9a175e
RonakKanji <67311728+RonakKanji@users.noreply.github.com>
2020-09-10 15:06:50 +0530

    Adding currency in CalculateRate.jsx

--- src/components/HomeLoanCalculator/CaclulateRates.jsx
+++ src/components/HomeLoanCalculator/CaclulateRates.jsx
@@ -1,25 +1,49 @@
 import React from "react";
 import PieChart from "./PieChart";
+import NumberFormat from "react-number-format";
 
-function CalculateRates(props){
-  return <div>
-    <div className="framing">
+function CalculateRates(props) {
+  return (
+    <div>
+      <div className="framing">
         <p>Loan EMI</p>
-        <p>{props.emi}</p>
+        <NumberFormat
+          displayType={"text"}
+          thousandSeparator={true}
+          thousandsGroupStyle="lakh"
+          prefix={"₹"}
+          value={props.emi}
+        />
       </div>
       <hr />
       <div className="framing">
         <p>Total Interest Payable</p>
-        <p>{props.totalEMI}</p>
+        <NumberFormat
+          displayType={"text"}
+          thousandSeparator={true}
+          thousandsGroupStyle="lakh"
+          prefix={"₹"}
+          value={props.totalEMI}
+        />
       </div>
       <hr />
       <div className="framing">
         <p>Total Payment</p>
-        <p>{props.payment}</p>
+        <NumberFormat
+          displayType={"text"}
+          thousandSeparator={true}
+          thousandsGroupStyle="lakh"
+          prefix={"₹"}
+          value={props.payment}
+        />
       </div>
       <hr />
-      <PieChart principalAmount={props.homeLoanAmount} totalInterest={props.payments} />
-  </div>;
+      <PieChart
+        principalAmount={props.homeLoanAmount}
+        totalInterest={props.payments}
+      />
+    </div>
+  );
 }
 
 export default CalculateRates;
